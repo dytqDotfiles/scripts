3ca9f5ac1aa2b7d9f712ca57a28ee13d230826fa
taariq <taariq.deda@gmail.com>
2020-11-22 09:54:27 +0100

    first commit

---
+++ volume_mute.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+INCR=10
+BASE="/usr/local/share/icons/gnome/"
+SIZE="16x16"
+ICON_MUTE="/status/audio-volume-muted.png"
+ICON_UNMUTE="/status/audio-volume-high.png"
+
+amixer sset Master toggle >/dev/null 2>&1
+
+CMD_AMIX=$(amixer get Master 2>&1)
+
+CMD_AMIX=$(echo $CMD_AMIX | grep "off" 2>&1)
+
+if [ -n "$CMD_AMIX" ]
+then
+	notify-send -i ""$BASE""$SIZE""$ICON_MUTE"" -t 1000 "volume mute"
+else
+	notify-send -i ""$BASE""$SIZE""$ICON_UNMUTE"" -t 1000 "volume unmute"
+fi
---
+++ volume_up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+INCR=10
+BASE="/usr/local/share/icons/gnome/"
+SIZE="16x16"
+ICON="/status/audio-volume-high.png"
+
+amixer sset Master unmute "$INCR"%+ >/dev/null 2>&1
+notify-send -i ""$BASE""$SIZE""$ICON"" -t 1000 "volume up"
