ab5586c1e74b2e0ac0c24447ea65988ce237a28e
taariq <taariq.deda@gmail.com>
2020-11-22 17:54:24 +0100

    add pourcentage

--- volume_down.sh
+++ volume_down.sh
@@ -5,5 +5,7 @@ BASE="/usr/local/share/icons/gnome/"
 SIZE="16x16"
 ICON="/status/audio-volume-low.png"
 
+AMIX_POURCENT=$(amixer sget Master | grep "%" | cut -f 6 -d ' ')
+
 amixer sset Master unmute "$INCR"%- >/dev/null 2>&1
-notify-send -i ""$BASE""$SIZE""$ICON"" -t 200 "volume down" 
+notify-send -i ""$BASE""$SIZE""$ICON"" -t 200 "volume down $AMIX_POURCENT" 
--- volume_mute.sh
+++ volume_mute.sh
@@ -9,12 +9,13 @@ ICON_UNMUTE="/status/audio-volume-high.png"
 amixer sset Master toggle >/dev/null 2>&1
 
 CMD_AMIX=$(amixer get Master 2>&1)
-
 CMD_AMIX=$(echo $CMD_AMIX | grep "off" 2>&1)
+
+AMIX_POURCENT=$(amixer sget Master | grep "%" | cut -f 6 -d ' ')
 
 if [ -n "$CMD_AMIX" ]
 then
-	notify-send -i ""$BASE""$SIZE""$ICON_MUTE"" -t 200 "volume mute"
+	notify-send -i ""$BASE""$SIZE""$ICON_MUTE"" -t 500 "volume mute"
 else
-	notify-send -i ""$BASE""$SIZE""$ICON_UNMUTE"" -t 200 "volume unmute"
+	notify-send -i ""$BASE""$SIZE""$ICON_UNMUTE"" -t 500 "volume unmute $AMIX_POURCENT"
 fi
